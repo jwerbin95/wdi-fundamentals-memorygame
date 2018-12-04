c80f9444bcbe3cdf013eb0d3f64d7bd5ee81d6c8
jwerbin95 <jwerbin95@gmail.com>
2018-12-04 12:31:04 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,21 @@
 console.log("Running!");
 var cards = ["Queen","Queen","King","King"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-if(cardsInPlay.length === 2)
+var flipCard = function(cardId)
 {
-	if(cardsInPlay[0] === cardsInPlay[1])
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+var checkForMatch = function()
+{
+	if(cardsInPlay.length === 2)
 	{
-		alert("You found a match!");
-	}
-	else
-	{
-		alert("Try again");
+		if(cardsInPlay[0] === cardsInPlay[1])
+			alert("You found a match!");
+		else
+			alert("Try again");
 	}
 }
+flipCard(0);
+flipCard(2);
+checkForMatch();
